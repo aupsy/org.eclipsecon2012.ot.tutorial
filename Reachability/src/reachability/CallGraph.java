04e9bc2bd2c4c9def29b62d5fc136e16e854656c
Stephan Herrmann <sherrmann>
2012-03-21 00:38:21 +0100

    Step 3: Create a nested team: - it's a role intercepting JavaBuilder.buildAll() - it's a team that is dynamically activated/deactivated This team defines when the analysis should be active.
    Don't forget to wire also this team, but don't activate an start.

--- Reachability/src/reachability/CallGraph.java
+++ Reachability/src/reachability/CallGraph.java
@@ -1,9 +1,25 @@
 package reachability;
 
+import base org.eclipse.jdt.internal.core.builder.JavaBuilder;
+
 /** 
  * This team adapts classes from JDT/Core 
  * to perform reachability analysis during each full build. 
  */
+@SuppressWarnings("restriction")
 public team class CallGraph {
 
+	/** Nested team that defines the context when our analysis should be active. */
+	protected team class LifeCycle playedBy JavaBuilder {
+
+		/** Intercept JavaBuilder.buildAll() to define the window of activity. */
+		void buildAll() <- replace void buildAll();
+
+		callin void buildAll() {
+			// perform the actual build with this nested team being active:
+			this.activate(ALL_THREADS);
+			base.buildAll();
+			this.deactivate(ALL_THREADS);	
+		}
+	}
 }
